78aa4a4ac882c51ecb1f251ac9a1a1dc14abcd0d
anime-kun32 <160499103+anime-kun32@users.noreply.github.com>
2024-07-15 06:33:22 +0100

    Update firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQS_LzzBI0VhUhHjmBqXDYfyx3wEyL80Y",
+  authDomain: "aniteams-e6301.firebaseapp.com",
+  projectId: "aniteams-e6301",
+  storageBucket: "aniteams-e6301.appspot.com",
+  messagingSenderId: "896555726967",
+  appId: "1:896555726967:web:a523f75e52d5d8a6266e72",
+  measurementId: "G-7EMPMEZDQB"
+};
